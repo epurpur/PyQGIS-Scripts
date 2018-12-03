7b954f326894fd05bea11ee93846ed74e38cb130
Erich Purpur <36010345+epurpur@users.noreply.github.com>
2018-12-03 15:16:50 -0500

    Add files via upload

--- UVAOncologyPatientData.py
+++ UVAOncologyPatientData.py
@@ -1,30 +1,29 @@
-
 from PyQt5.QtGui import QColor
 from PyQt5.QtCore import QVariant
 from qgis.PyQt.QtCore import QVariant
-#from PyQt5.QtCore import Qt
-#from qgis.utils import iface
 
-#join_layer = QgsVectorLayer('/Users/ep9k/Desktop/SandraMonson/cb_2017_us_zcta510_500k/cb_2017_us_zcta510_500k.shp', 'US Zip Codes', 'ogr')
+
 uri = '/Users/ep9k/Desktop/SandraMonson/cb_2017_us_zcta510_500k/cb_2017_us_zcta510_500k.shp'
-join_layer = iface.addVectorLayer(uri, 'Zip Codez', 'ogr')
-
+join_layer = iface.addVectorLayer(uri, 'Patients by Zip Code', 'ogr')
+target_field = 'PatCNT'
 
 def add_csv():
     """Adds csv file of patient data to map. Pop-up dialog box prompts user to input file path
     QInputDialog prompts user for file name"""
-    
-    file_name = QInputDialog.getText(None, 'Enter input filepath', 'Please save patient data as csv and paste full pathname here. (Example: /Users/ep9k/Desktop/SandraMonson/TestZips.csv)')
-    file_name = file_name[0]
 
-    uri = f"file://{file_name}?delimiter=,'"    #don't know why, but it needs file:// before path to csv  
+    file_name = QInputDialog.getText(None, 'Enter input filepath to csv file', 'Please save patient data as csv and paste full pathname here. (Example: /Users/ep9k/Desktop/SandraMonson/TestZips.csv)')
+    file_name = file_name[0]        #QInputDialog returns a tuple, this is first object of tuple, which is a string of the file name
+
+    uri = f"file://{file_name}?delimiter=,'"    #needs file:// before path to csv  
     
     info_layer = QgsVectorLayer(uri, 'Patient_Data', 'delimitedtext')
     if info_layer.isValid():
-        print("info_layer is valid")
+        print("info_layer is valid. Adding csv to map")
         QgsProject.instance().addMapLayer(info_layer)
     else:
         print("Invalid csv file. Please check your file path. (uri variable)")
+
+    return info_layer
 
 
 def join_tables(join_layer, info_layer):
@@ -35,7 +34,7 @@ def join_tables(join_layer, info_layer):
     
     csvField = 'ZipCode'
     shpField = 'GEOID10'
-    joinObject=QgsVectorLayerJoinInfo()
+    joinObject = QgsVectorLayerJoinInfo()
     joinObject.setJoinFieldName(csvField)
     joinObject.setTargetFieldName(shpField)
     joinObject.setJoinLayerId(info_layer.id())
@@ -62,14 +61,47 @@ def calculate_attributes():
             join_layer.updateFeature(feature)
 
 
-def change_color():
-    """Changes symbology of zip codes. Currently single symbol. Will be graduated based on PatCNT in future"""
-    
-    renderer = join_layer.renderer()
-    symbol = renderer.symbol()
-    symbol.setColor(QColor(Qt.black))
-    join_layer.triggerRepaint()
-    iface.layerTreeView().refreshLayerSymbology(join_layer.id())
+def apply_graduated_symbology():
+    """Creates Symbology for each value in range of values. 
+        Values are # of patients per zip code.
+        Hard codes min value, max value, symbol (color), and label for each range of values.
+        Then QgsSymbolRenderer takes field from attribute table and item from myRangeList and applies them to join_layer"""
+    myRangeList = []
+
+    symbol = QgsSymbol.defaultSymbol(join_layer.geometryType())
+    symbol.setColor(QColor("#f5c9c9"))
+    myRange = QgsRendererRange(0, 1, symbol, '1')
+    myRangeList.append(myRange)
+
+    symbol = QgsSymbol.defaultSymbol(join_layer.geometryType())
+    symbol.setColor(QColor("#f97a7a"))
+    myRange = QgsRendererRange(1.1, 2, symbol, '2')
+    myRangeList.append(myRange)
+
+    symbol = QgsSymbol.defaultSymbol(join_layer.geometryType())
+    symbol.setColor(QColor("#ff0000"))
+    myRange = QgsRendererRange(2.1, 3, symbol, '3')
+    myRangeList.append(myRange)
+
+    symbol = QgsSymbol.defaultSymbol(join_layer.geometryType())
+    symbol.setColor(QColor("#8a0000"))
+    myRange = QgsRendererRange(3.1, 4, symbol, '4')
+    myRangeList.append(myRange)
+
+    symbol = QgsSymbol.defaultSymbol(join_layer.geometryType())
+    symbol.setColor(QColor("#4a0000"))
+    myRange = QgsRendererRange(4.1, 5, symbol, '5')
+    myRangeList.append(myRange)
+
+    symbol = QgsSymbol.defaultSymbol(join_layer.geometryType())
+    symbol.setColor(QColor("#000000"))
+    myRange = QgsRendererRange(5.1, 6, symbol, '6 or more patients')
+    myRangeList.append(myRange)
+
+    myRenderer = QgsGraduatedSymbolRenderer(target_field, myRangeList)
+    myRenderer.setMode(QgsGraduatedSymbolRenderer.Custom)
+
+    join_layer.setRenderer(myRenderer)
 
 
 def main_module():
@@ -78,45 +110,7 @@ def main_module():
     join_tables(join_layer, info_layer)
     add_column_to_attribute_table()
     calculate_attributes()
-    change_color()
+    apply_graduated_symbology()
 
 
 main_module()
-
-
-"""SAVING THIS FOR LATER, UPLOADED TO GITHUB
-target_field = 'PatCNT'
-layer = QgsVectorLayer('/Users/ep9k/Desktop/SandraMonson/TestZips.shp', 'Fixed Divisions', 'ogr')
-
-def validatedDefaultSymbol(geometryType):
-    symbol = QgsSymbol.defaultSymbol(geometryType)
-    if symbol is None:
-        if geometryType == Qgis.Point:
-            symbol = QgsMarkerSymbol()
-        elif geometryType == Qgis.Line:
-            symbol = QgsLineSymbol()
-        elif geometryType == Qgis.Polygon:
-            symbol = QgsFillSymbol()
-    print("Symbol type = ", type(symbol))
-    return symbol
-
-def makeSymbologyForRange(layer, min, max, title, color):
-    symbol = validatedDefaultSymbol(layer.geometryType())
-    symbol.setColor(color)
-    range = QgsRendererRange(min, max, symbol, title)
-    return range
-
-def applySymbologyFixedDivisions(layer, field):
-    rangeList = []
-    rangeList.append( makeSymbologyForRange(layer, 2, 4, '2-4', QColor("Green") ) )
-    rangeList.append( makeSymbologyForRange(layer, 4.1, 6, '4-6',  QColor("Purple") ) )
-    renderer = QgsGraduatedSymbolRenderer(field, rangeList)
-    renderer.setMode(QgsGraduatedSymbolRenderer.Custom)
-    layer.setRenderer(renderer)
-
-if layer.isValid():
-    applySymbologyFixedDivisions(layer, target_field)
-    QgsProject.instance().addMapLayer(layer)
-"""
-
-
