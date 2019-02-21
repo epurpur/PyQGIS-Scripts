24545d9873dd050bf244d363a75ebf843c689c30
Erich Purpur <36010345+epurpur@users.noreply.github.com>
2019-02-21 16:25:02 -0500

    Add files via upload

--- EuclideanDistance3D.py
+++ EuclideanDistance3D.py
@@ -1,3 +1,10 @@
+"""Measures Euclidean Distance in 3 Dimensions (straight line distance).
+Takes x, y, z values for 2 points and calculates distance between them via Euclidean distance formula.
+
+Example:
+a = (2321.4 (latitude), -4747.62 (longitude), 750.5 (altitude))
+"""
+
 from math import sqrt
 
 a = (2321.4, -4747.62, 750.5)
