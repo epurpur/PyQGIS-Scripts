7722e35f08853c2e8a9c61483c43f80eeee1a2f4
Erich Purpur <epurpur@gmail.com>
2019-05-25 12:01:10 -0400

    Add files via upload

---
+++ GRASSvIDWsurf.py
@@ -0,0 +1,18 @@
+"""Code for GRASS v.idw.surf tool. Must use QGIS v3.6 for this to work"""
+
+import processing
+
+parameters = {
+    'input' : '/Users/ep9k/Desktop/qgis_data-master/area3_testdata_clipped.shp',
+    'npoints' : 12,
+    'power' : 2,
+    'column' : 'PDOP',
+    'GRASS_REGION_PARAMETER' : '-116.04415744105579,-116.0047567429083,37.029018114312905,37.054281406967974 [EPSG:4326]',
+    'GRASS_REGION_CELLSIZE_PARAMETER' : 0,
+    'GRASS_RASTER_FORMAT_OPT' : '',
+    'GRASS_RASTER_FORMAT_META' : '',
+    'GRASS_SNAP_TOLERANCE_PARAMETER' : -1,
+    'GRASS_MIN_AREA_PARAMETER' : 0.0001,
+    'output' : '/Users/ep9k/Desktop/GRASS_OUTPUT.tif'}
+
+processing.runAndLoadResults('grass7:v.surf.idw', parameters)
